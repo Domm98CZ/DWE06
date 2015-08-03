eac9c1cb8a2a79ae2c94696b7dd06386bc797c04
Dominik Procházka <domm98@domm98.cz>
2015-08-03 17:05:38 +0200

    Index Fix

--- index.php
+++ index.php
@@ -26,16 +26,6 @@ require_once "_core/main.php";
     <link href="assets/css/main.css" rel="stylesheet">
     <link rel="stylesheet" href="assets/css/font-awesome.min.css">
     <meta name="generator" content="Domm Web Engine <?php echo Web_GetVersion();?>" />
-    <script>
-      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    
-      ga('create', 'UA-46006794-6', 'auto');
-      ga('send', 'pageview');
-    
-    </script>
   </head>
   <body>
     <nav class="navbar navbar-<?php echo Web_GetOption("MENU");?> navbar-fixed-top" role="navigation">
